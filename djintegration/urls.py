95d4004299b0c218ea644fde6d2ff0c560e1239b
Batiste Bieler <batisteb@opera.com>
2010-02-24 11:40:57 +0100

    Add repository tests list view.

--- djintegration/urls.py
+++ djintegration/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('djintegration.views',
     url(r'^$', 'lastest_reports', name="latest-reports"),
+    url(r'^repo/(?P<repo_id>[0-9]+)$', 'repository', name="repository"),
 )
--- djintegration/views.py
+++ djintegration/views.py
@@ -10,3 +10,11 @@ def lastest_reports(request):
     
     return render_to_response('djintegration/latest_reports.html',
         RequestContext(request, locals()))
+
+def repository(request, repo_id):
+
+    repo = Repository.objects.get(pk=int(repo_id))
+    tests = TestReport.objects.filter(repository=repo).order_by('-creation_date')[0:30]
+
+    return render_to_response('djintegration/repository.html',
+        RequestContext(request, locals()))
